91eca37e3a02501d3901bca18047e8775b7c4306
jadanguyend <jadanguyend@gmail.com>
2025-11-27 17:08:46 -0800

    Added Tailwin

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-5xl font-bold text-blue-600">Tailwind is working!</h1>
+    </div>
+  );
+}
